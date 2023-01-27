db362608a8bf2611de8852b0c8e10a9169b7587f
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-01-27 09:17:19 +0900

    feat: template method

---
+++ src/main/java/com/design/pattern/templateMethod/CharDisplay.java
@@ -0,0 +1,24 @@
+package com.design.pattern.templateMethod;
+
+public class CharDisplay extends AbstractDisplay {
+    private char ch;
+
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("<<");
+    }
+
+    @Override
+    public void print() {
+        System.out.println(ch);
+    }
+
+    @Override
+    public void close() {
+        System.out.println(">>");
+    }
+}
---
+++ src/main/java/com/design/pattern/templateMethod/StringDisplay.java
@@ -0,0 +1,34 @@
+package com.design.pattern.templateMethod;
+
+public class StringDisplay extends AbstractDisplay {
+    private String string;
+    private int width;
+
+    public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.length();
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + string + "|");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.println("+");
+        for (int i = 0; i < width; i++) {
+            System.out.println("-");
+        }
+        System.out.println("+");
+    }
+}
