12a23732e7520241e6eefc807e850ff6c4ad802f
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-01-30 09:10:01 +0900

    feat: singleton

---
+++ src/main/java/com/design/pattern/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package com.design.pattern.singleton;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("インスタンスを生成");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
