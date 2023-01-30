825c16cb8fa3364b679bddd771010a34618ebe37
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-01-30 09:47:20 +0900

    feat: prototype

---
+++ src/main/java/com/design/pattern/prototype/MessageBox.java
@@ -0,0 +1,25 @@
+package com.design.pattern.prototype;
+
+public class MessageBox implements Product {
+    private char decochar;
+
+    public MessageBox(char decochar) {
+        this.decochar = decochar;
+    }
+
+    @Override
+    public void use(String s) {
+        System.out.println(s);
+    }
+
+    @Override
+    public Product createCopy() {
+        Product p = null;
+        try {
+            p = (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
---
+++ src/main/java/com/design/pattern/prototype/Product.java
@@ -0,0 +1,6 @@
+package com.design.pattern.prototype;
+
+public interface Product extends Cloneable {
+    public abstract void use(String s);
+    public abstract Product createCopy();
+}
