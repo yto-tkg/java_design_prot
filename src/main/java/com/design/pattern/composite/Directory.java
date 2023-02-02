757837c440d38288e1cd1401d17283c630f2b9ff
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-02-03 08:37:14 +0900

    feat: composite

---
+++ src/main/java/com/design/pattern/composite/Directory.java
@@ -0,0 +1,37 @@
+package com.design.pattern.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory extends Entry {
+    private String name;
+    private List<Entry> directory = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        int size = 0;
+        for (Entry entry : directory) {
+            size += entry.getSize();
+        }
+        return size;
+    }
+
+    @Override
+    protected void printList(String prefix) {
+        System.out.println(prefix + this);
+    }
+
+    public Entry add(Entry entry) {
+        directory.add(entry);
+        return this;
+    }
+}
---
+++ src/main/java/com/design/pattern/composite/Entry.java
@@ -0,0 +1,18 @@
+package com.design.pattern.composite;
+
+public abstract class Entry {
+    public abstract String getName();
+
+    public abstract int getSize();
+
+    public void printList() {
+        printList("");
+    }
+
+    protected abstract void printList(String prefix);
+
+    @Override
+    public String toString() {
+        return getName() + getSize();
+    }
+}
