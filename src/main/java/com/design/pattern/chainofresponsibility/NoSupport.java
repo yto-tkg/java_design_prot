72baf867b5856c62afa10cf7337c8f6e280f2f14
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-02-07 08:50:13 +0900

    feat: chain of responsibility

---
+++ src/main/java/com/design/pattern/chainofresponsibility/NoSupport.java
@@ -0,0 +1,12 @@
+package com.design.pattern.chainofresponsibility;
+
+public class NoSupport extends Support{
+    public NoSupport(String name) {
+        super(name);
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return false;
+    }
+}
---
+++ src/main/java/com/design/pattern/chainofresponsibility/Trouble.java
@@ -0,0 +1,13 @@
+package com.design.pattern.chainofresponsibility;
+
+public class Trouble {
+    private int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
